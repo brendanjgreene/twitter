a52365387991bd72bf10562cc458b4d65d89928f
Brendan Greene <brendanjgreene@gmail.com>
2017-01-30 21:05:08 +0000

    tweet access

---
+++ tweet_access.py
@@ -0,0 +1,19 @@
+import json
+import tweepy
+from tweepy import OAuthHandler
+from settings import CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN_SECRET, OAUTH_TOKEN
+
+# i have my keys and oauths to a settungs file for privacy
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+count = 10
+query = 'Dublin'
+
+# Get all status
+results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+
+for result in results:
+    print result
